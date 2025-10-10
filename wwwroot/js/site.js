fed781527a9397701b644b0b23fda4fced7679bd
Tristanlake45 <tristan.lake@southeasttech.edu>
2025-10-10 09:17:28 -0500

    Responsive header, table badges, mobile view updates

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Shrink header on scroll
+(function () {
+    const header = document.querySelector('.app-header');
+    if (!header) return;
+  
+    let ticking = false;
+    function onScroll() {
+      if (!ticking) {
+        window.requestAnimationFrame(function () {
+          const y = window.scrollY || document.documentElement.scrollTop;
+          if (y > 12) header.classList.add('shrink');
+          else header.classList.remove('shrink');
+          ticking = false;
+        });
+        ticking = true;
+      }
+    }
+  
+    window.addEventListener('scroll', onScroll, { passive: true });
+    onScroll();
+  })();
+  
